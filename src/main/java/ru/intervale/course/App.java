3f2921f7c4cca74daffaa2e7f2d3e27b935c0992
Yauheni Reut <reut07@rambler.ru>
2018-05-03 18:55:34 +0300

    initial commit

---
+++ src/main/java/ru/intervale/course/App.java
@@ -0,0 +1,13 @@
+package ru.intervale.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
