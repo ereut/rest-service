0e5c29c1769f50f3f33c21bd71ec6216b781af49
Yauheni Reut <reut07@rambler.ru>
2018-05-07 19:37:05 +0300

    before refactoring

--- src/main/java/ru/intervale/course/Constants.java
+++ src/main/java/ru/intervale/course/Constants.java
@@ -4,14 +4,32 @@ public class Constants {
 
     public static final String DATA_BASE_DAO =
             ApplicationContext.getPropertyValue("Ddatabase");
-    public static final String MY_SQL_DRIVAER_CLASS_NAME =
-            ApplicationContext.getPropertyValue("DmySQLDriverClassName");
     public final static String DBURL =
             ApplicationContext.getPropertyValue("Ddburl");
     public final static String USER_NAME =
             ApplicationContext.getPropertyValue("Ddbuser");
     public final static String USER_PASSWORD =
             ApplicationContext.getPropertyValue("Ddbpassword");
+
+    public final static String CREATE_CARDS_TABLE_QUERY =
+            "CREATE TABLE `customers`.`cards` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `customerId` INT NOT NULL,\n" +
+                    "  `pan` VARCHAR(45) NOT NULL,\n" +
+                    "  `expiry` DATE NOT NULL,\n" +
+                    "  `registerTime` DATETIME NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`));\n";
+
+    public final static String CREATE_PAYMENTS_TABLE_QUERY =
+            "CREATE TABLE `customers`.`payments` (\n" +
+                    "  `id` INT NOT NULL,\n" +
+                    "  `cardId` INT NOT NULL,\n" +
+                    "  `startTime` DATETIME NOT NULL,\n" +
+                    "  `pfinishTime` DATETIME NOT NULL,\n" +
+                    "  `value` INT NOT NULL,\n" +
+                    "  `currency` VARCHAR(45) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`),\n" +
+                    "  UNIQUE INDEX `id_UNIQUE` (`id` ASC));";
 
     public final static String CREATE_CUSTOMERS_TABLE_QUERY =
             ApplicationContext.getPropertyValue("QcreateCustomersTable");
--- src/main/java/ru/intervale/course/Runner.java
+++ src/main/java/ru/intervale/course/Runner.java
@@ -11,25 +11,7 @@ public class Runner {
 
     public static void main(String args[]) {
 
-        final String CREATE_CARDS_TABLE_QUERY =
-                "CREATE TABLE `customers`.`cards` (\n" +
-                        "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                        "  `customerId` INT NOT NULL,\n" +
-                        "  `cardPan` VARCHAR(45) NOT NULL,\n" +
-                        "  `cardExpiry` DATE NOT NULL,\n" +
-                        "  `cardRegisterTime` DATETIME NOT NULL,\n" +
-                        "  PRIMARY KEY (`id`));\n";
 
-        final String CREATE_PAYMENTS_TABLE_QUERY =
-                "CREATE TABLE `customers`.`payments` (\n" +
-                        "  `id` INT NOT NULL,\n" +
-                        "  `cardId` INT NOT NULL,\n" +
-                        "  `paymentStartTime` DATETIME NOT NULL,\n" +
-                        "  `paymentFinishTime` DATETIME NOT NULL,\n" +
-                        "  `paymentValue` INT NOT NULL,\n" +
-                        "  `paymentCurrency` VARCHAR(45) NOT NULL,\n" +
-                        "  PRIMARY KEY (`id`),\n" +
-                        "  UNIQUE INDEX `id_UNIQUE` (`id` ASC));";
 
         try (Connection cn = MySQLConnection.getMySQLConnection()) {
            PreparedStatement pst = cn.prepareStatement(Constants.CREATE_CUSTOMERS_TABLE_QUERY);
