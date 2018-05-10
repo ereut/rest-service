c48ab5879fb9c220758d8ada0db4c6f6625b10d1
Женя-ПК\Женя <reut07@rambler.ru>
2018-05-10 22:32:58 +0300

    tests added

---
+++ src/test/java/ru/intervale/course/dao/AbstractJDBCDaoTest.java
@@ -0,0 +1,30 @@
+package ru.intervale.course.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.junit.Assert.*;
+
+public class AbstractJDBCDaoTest {
+    @Test
+    public void getEntityById() throws Exception {
+        final String GET_ID_QUERY = "SELECT id FROM customers.customers";
+
+        try (Connection cn =
+                     DriverManager.getConnection("jdbc:h2:tcp://localhost/~/customers", "sa","");
+             PreparedStatement pst = cn.prepareStatement(GET_ID_QUERY)) {
+
+            ResultSet rs = pst.executeQuery();
+            rs.next();
+            int id = rs.getInt(1);
+            Assert.assertNotNull(new CustomerJDBCDao(cn).getEntityById(id));
+
+        }
+    }
+
+}
