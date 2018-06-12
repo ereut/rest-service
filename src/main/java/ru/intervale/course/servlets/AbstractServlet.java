580590f1ef76b5f697797c8662b9f5c821127c03
Женя-ПК\Женя <reut07@rambler.ru>
2018-06-12 22:36:53 +0300

    Responce status codes enum added

--- src/main/java/ru/intervale/course/servlets/AbstractServlet.java
+++ src/main/java/ru/intervale/course/servlets/AbstractServlet.java
@@ -1,5 +1,6 @@
 package ru.intervale.course.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.intervale.course.beans.AbstractEntity;
@@ -8,20 +9,26 @@ import ru.intervale.course.dao.IDao;
 import ru.intervale.course.impl.AbstractJDBCDaoImpl;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/*")
 public abstract class AbstractServlet<T extends AbstractEntity, K extends AbstractJDBCDaoImpl<T>> extends HttpServlet {
+
+    private enum Operations {
+        ADD, DELETE, UPDATE;
+    }
 
     private static Logger logger = LoggerFactory.getLogger(AbstractServlet.class);
     private IDao<T> iDaoImpl;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
-    private static String PROBLEMS_WITH_CREATION_DAO_IMPL_MESSAGE =
+    private static final String PROBLEMS_WITH_CREATION_DAO_IMPL_MESSAGE =
             "Problems with creation dao implementation";
+    private static final String RESPONCE_CHARACTER_ENCODING = "UTF-8";
+    private static final String RESPONCE_CONTENT_TYPE = "application/json";
 
     @Override
     public void init() throws ServletException {
@@ -50,4 +57,11 @@ public abstract class AbstractServlet<T extends AbstractEntity, K extends Abstra
     }
 
     public abstract IDao<T> getDaoImpl() throws DaoException;
+
+    private static PrintWriter getPrintWriterFromResp(HttpServletResponse resp) throws IOException {
+        resp.setCharacterEncoding(RESPONCE_CHARACTER_ENCODING);
+        resp.setContentType(RESPONCE_CONTENT_TYPE);
+        return resp.getWriter();
+    }
 }
+
